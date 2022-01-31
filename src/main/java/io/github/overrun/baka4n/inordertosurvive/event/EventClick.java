b083c359057551aea9f7e93c0786c98e06d0dfaa
baka4n <qq474899581@gmail.com>
2022-02-01 04:01:20 +0800

    Update EventClick.java
    add break event settings

--- src/main/java/io/github/overrun/baka4n/inordertosurvive/event/EventClick.java
+++ src/main/java/io/github/overrun/baka4n/inordertosurvive/event/EventClick.java
@@ -1,19 +1,24 @@
 package io.github.overrun.baka4n.inordertosurvive.event;
 
-import io.github.overrun.baka4n.inordertosurvive.InOrderToSurvive;
 import io.github.overrun.baka4n.inordertosurvive.registry.AllRegistry;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.material.Material;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+import javax.annotation.Nullable;
 
 /**
  * @author baka4n
@@ -22,23 +27,19 @@ import net.minecraftforge.fml.common.Mod;
  * I'm sorry you grinded the flint too hard
  * you have a 70% chance of getting it
  */
-@Mod.EventBusSubscriber(Dist.CLIENT)
+@EventBusSubscriber(Dist.CLIENT)
 public class EventClick {
-	@SubscribeEvent
-	public static void LookingBlocks() {
-
-	}
 
 	@SubscribeEvent
 	public static void rightClient(PlayerInteractEvent.RightClickBlock event) {
-
 		ItemStack stack = event.getItemStack();
 		int value = event.getItemStack().getCount();
 		int random = (int) (Math.random() * 100);
 		int a = value - 1;
+		Level world = event.getWorld();
+		BlockPos blockPos = event.getPos();
 		Player entityPlayer = event.getPlayer();
-
-		if (stack.getItem().asItem().toString().equals(Items.FLINT.toString())) {
+		if (stack.getItem().asItem().toString().equals(Items.FLINT.toString()) && world.getBlockState(blockPos).getMaterial() == Material.STONE) {
 			if (value != 1) {
 				ItemStack handStack = new ItemStack(Items.FLINT.asItem(), a);
 
@@ -56,6 +57,19 @@ public class EventClick {
 					entityPlayer.drop(dropStack, true);
 				}
 			}
+		}
+
+	}
+	// Blocks cannot be broken in the early stage. You may have to make flint tools to break them.
+	// 未完工
+	@SubscribeEvent
+	public static void Break(BlockEvent.BreakEvent event) {
+		Item handItem = event.getPlayer().getItemInHand(InteractionHand.MAIN_HAND).getItem().asItem();
+		ItemStack stack = new ItemStack(handItem);
+		LevelAccessor world = event.getWorld();
+		BlockPos blockPos = event.getPos();
+		if (!world.getBlockState(blockPos).getBlock().asItem().toString().equals(Items.GRAVEL.toString())) {
+			event.setCanceled(true);
 		}
 
 	}
