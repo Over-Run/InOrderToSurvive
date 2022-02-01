bac605c07e082e3fcc49dc5a4cb30da329446256
baka4n <qq474899581@gmail.com>
2022-02-01 15:47:54 +0800

    change gravel loot table and breakBlockEvent

--- src/main/java/io/github/overrun/baka4n/inordertosurvive/event/EventClick.java
+++ src/main/java/io/github/overrun/baka4n/inordertosurvive/event/EventClick.java
@@ -1,24 +1,26 @@
 package io.github.overrun.baka4n.inordertosurvive.event;
 
+import io.github.overrun.baka4n.inordertosurvive.InOrderToSurvive;
 import io.github.overrun.baka4n.inordertosurvive.registry.AllRegistry;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.material.Material;
+
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
 
-import javax.annotation.Nullable;
+import static net.minecraft.world.InteractionHand.MAIN_HAND;
+import static net.minecraft.world.item.Items.*;
+
 
 /**
  * @author baka4n
@@ -29,7 +31,6 @@ import javax.annotation.Nullable;
  */
 @EventBusSubscriber(Dist.CLIENT)
 public class EventClick {
-
 	@SubscribeEvent
 	public static void rightClient(PlayerInteractEvent.RightClickBlock event) {
 		ItemStack stack = event.getItemStack();
@@ -39,18 +40,18 @@ public class EventClick {
 		Level world = event.getWorld();
 		BlockPos blockPos = event.getPos();
 		Player entityPlayer = event.getPlayer();
-		if (stack.getItem().asItem().toString().equals(Items.FLINT.toString()) && world.getBlockState(blockPos).getMaterial() == Material.STONE) {
+		if (stack.getItem().asItem().toString().equals(FLINT.toString()) && world.getBlockState(blockPos).getMaterial() == Material.STONE) {
 			if (value != 1) {
-				ItemStack handStack = new ItemStack(Items.FLINT.asItem(), a);
+				ItemStack handStack = new ItemStack(FLINT.asItem(), a);
 
-				entityPlayer.setItemInHand(InteractionHand.MAIN_HAND, handStack);
+				entityPlayer.setItemInHand(MAIN_HAND, handStack);
 				if (random <= 70) {
 					ItemStack dropStack = new ItemStack(AllRegistry.ItemRegistry.flint_flakes.get().asItem());
 					entityPlayer.drop(dropStack, true);
 				}
 			} else {
-				ItemStack handStack = new ItemStack(Items.AIR.asItem());
-				entityPlayer.setItemInHand(InteractionHand.MAIN_HAND, handStack);
+				ItemStack handStack = new ItemStack(AIR.asItem());
+				entityPlayer.setItemInHand(MAIN_HAND, handStack);
 
 				if (random <= 70) {
 					ItemStack dropStack = new ItemStack(AllRegistry.ItemRegistry.flint_flakes.get().asItem());
@@ -58,19 +59,50 @@ public class EventClick {
 				}
 			}
 		}
-
 	}
 	// Blocks cannot be broken in the early stage. You may have to make flint tools to break them.
 	// 未完工
 	@SubscribeEvent
 	public static void Break(BlockEvent.BreakEvent event) {
-		Item handItem = event.getPlayer().getItemInHand(InteractionHand.MAIN_HAND).getItem().asItem();
-		ItemStack stack = new ItemStack(handItem);
+		/*
+		get player
+		get my main hand item
+		get my main hand item stack
+		 */
+		Player eventPlayer = event.getPlayer();
+		Item handItem = eventPlayer.getItemInHand(MAIN_HAND).getItem().asItem();
+		Item stack = new ItemStack(handItem).getItem().asItem();
+		/*
+		  get world
+		  get break world block pos
+		  give break world block item
+		 */
 		LevelAccessor world = event.getWorld();
 		BlockPos blockPos = event.getPos();
-		if (!world.getBlockState(blockPos).getBlock().asItem().toString().equals(Items.GRAVEL.toString())) {
-			event.setCanceled(true);
-		}
+		Item item = world.getBlockState(blockPos).getBlock().asItem();
 
+		// main hand item don't have air
+		boolean b = !stack.toString().equals(AIR.toString());
+		//random
+		int random = (int) (Math.random() * 100);
+		event.setCanceled(!item.equals(GRAVEL)
+				&& !ifBool(item, ACACIA_LEAVES, b)
+				&& !ifBool(item, BIRCH_LEAVES, b)
+				&& !ifBool(item, DARK_OAK_LEAVES, b)
+				&& !ifBool(item, JUNGLE_LEAVES, b)
+				&& !ifBool(item, SPRUCE_LEAVES, b)
+				&& !ifBool(item, OAK_LEAVES, b));
+	}
+	public static boolean ifBool(Item item,Item item2, boolean b) {
+		return item.equals(item2) && b;
+	}
+	public static void dropLeaves(int ran_min,int ran_max,Player player, ItemStack stack, ItemStack stack2, ItemStack stack3) {
+		if ((int) (Math.random() * 100) <=ran_min) {
+			player.drop(stack, true);
+		} else if ((int) (Math.random() * 100) >= ran_max) {
+			player.drop(stack2, true);
+		} else {
+			player.drop(stack3,true);
+		}
 	}
 }
